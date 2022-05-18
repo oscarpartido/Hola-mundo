406e9947e1fc0a13d9b15c36e070d53ff4159260
oscarpartido <oscarpartido01@gmail.com>
2022-05-17 21:50:30 -0500

    commit del cliente
    modifique el readme con mas detalles
    agregue el archivo hola mundo.cpp

--- Hola mundo.cpp
+++ Hola mundo.cpp
@@ -1,11 +1,20 @@
+/*programa que muestra la estructura basica de C
 
 Autor:oscar vivaldi partido ramirez 
 escuela: uvm campus Queretaro
 materia: Programacion Estructurada
 ciclo:01/2022
 
-programa que muestra la estructura b√°sica de un programa en el lenguaje C 
+programa que muestra la estructura b·sica de un programa en el lenguaje C 
 ademas se ve el uso de:
 -include . para agregar librerias 
 -comentarios. para documentacion interna del programa
 -printf. para datos de pantalla 
+*/
+//incluir librerias 
+#include<stdio.h>
+//programa principal
+int main(){
+	printf("hola mundo \n");
+	return 0;
+}
